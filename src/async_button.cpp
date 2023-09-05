3fe636c75cb938297f8418a32808207cb90c1dab
KOEGlike <KOEGlike@users.noreply.github.com>
2023-09-05 23:27:32 +0300

    v1.0.0

---
+++ src/async_button.cpp
@@ -0,0 +1,42 @@
+#include "async_button.hpp"
+
+#include <Arduino.h>
+#include <functional>
+
+Button::Button(int pin, std::function<void()> press, std::function<void()> longPress, bool* globalPressed) {
+    m_pin = pin;
+    m_globalPressed = globalPressed;
+    m_longPress = longPress;
+    m_press = press;
+    pinMode(m_pin, INPUT_PULLUP);
+}
+
+void Button::check() {
+    if (digitalRead(m_pin) == HIGH) {
+        if (pressed == true) {
+            pressedForMillis = millis() - pressStartInMillies;
+        }
+        pressed = false;
+        *m_globalPressed = false;
+    } else {
+        if (pressed == false) {
+            pressStartInMillies = millis();
+        }
+        if (*m_globalPressed == false) {
+            pressed = true;
+            *m_globalPressed = true;
+        }
+    }
+
+    if (pressed == false && *m_globalPressed == false && pressedForMillis >= debounceInMillis) {
+        if (pressedForMillis < millisForLongPress) {
+            m_press();
+        } else {
+            m_longPress();
+        }
+    }
+
+    if (pressed == false) {
+        pressedForMillis = 0;
+    }
+}
---
+++ src/async_button.hpp
@@ -0,0 +1,19 @@
+#ifndef BUTTON_HPP
+#define BUTTON_HPP
+#include <functional>
+
+class Button {
+protected:
+    int m_pin;
+    bool* m_globalPressed;
+    std::function<void()> m_longPress, m_press;
+    bool pressed = false;
+    unsigned long millisForLongPress = 300, debounceInMillis = 2, pressedForMillis = 0, pressStartInMillies;
+
+public:
+    Button(int pin, std::function<void()> press = []() {}, std::function<void()> longPress = []() {}, bool* globalPressed);
+    void check();
+
+};
+
+#endif
