3933da53a3acd32f4550932955cb3d7211a28556
KOEGlike <marcell.balazs@yahoo.com>
2023-11-14 13:24:13 +0100

    2.0

--- examples/AsyncButtonBlink/AsyncButtonBlink.ino
+++ examples/AsyncButtonBlink/AsyncButtonBlink.ino
@@ -1,14 +1,17 @@
 #include <Arduino.h>
-#include <async_button.hpp>
+#include <async_button.h>
 
-#define BUTTON_PIN 2
-#define LED_PIN 13
+const int btn1pin=0;//put your button pin here
+const int btn2pin=1;//put your button pin here
 
-AsyncButton btn (BUTTON_PIN,[](){digitalWrite(LED_PIN, HIGH);}, [](){digitalWrite(LED_PIN, LOW);}) ;
+
+AsyncButton btn1 (btn1pin,[&](){Serial.println("btn 1 short");}, [&](){Serial.println("btn1 long");});
+AsyncButton btn2 (btn2pin,[&](){Serial.println("btn 2 short");}, [&](){Serial.println("btn2 long");});
 
 void setup() {
+  ButtonManager.link(std::vector<AsyncButton>{btn1, btn2}, [&](){Serial.println("duble");});
 }
 
 void loop() {
-  btn.check();
+  ButtonManager.check();
 }
